735872ccd883ccb25f94000c4eb9995bf38d405f
jess <jess.monter@ciencias.unam.mx>
2021-07-08 00:02:07 -0500

    Remove redundant tests, simplify home view

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,5 +19,6 @@ from lists import views
 
 urlpatterns = [
    url(r'^$', views.home_page, name='home'),
-   url(r'^lists/mi-lista-chingona/$', views.view_list, name='view-list',)
+   url(r'^lists/new', views.new_list, name='new_list'),
+   url(r'^lists/mi-lista-chingona/$', views.view_list, name='view_list',)
 ]
